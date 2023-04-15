c8e127605bc29b095487627f9a70fa71942c4b03
Anik Ghosh <anikghosh46.bd@gmail.com>
2023-04-16 03:46:46 +0600

    Print helper and logo added.

---
+++ helpers/print.js
@@ -0,0 +1,10 @@
+exports.logo = () => {  
+  console.log(chalk.gray(' _    _ _   _         '))
+  console.log(chalk.gray('| |  (_) | | |        '))
+  console.log(chalk.gray('| | ___| |_| |_ _   _ '))
+  console.log(chalk.gray('| |/ / | __| __| | | |'))
+  console.log(chalk.gray('|   <| | |_| |_| |_| |'))
+  console.log(chalk.gray('|_|\_\_|\__|\__|\__, |'))
+  console.log(chalk.gray('                 __/ |'))
+  console.log(chalk.gray('                |___/ '))
+}
