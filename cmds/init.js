3eb30ec6fa8a1cac396b05f28117d28b13a40043
Anik Ghosh <anikghosh46.bd@gmail.com>
2023-04-16 03:47:23 +0600

    Init command done.

--- cmds/init.js
+++ cmds/init.js
@@ -1,15 +1,23 @@
-/**
- * Init command
- */
+const { copy } = require('../helpers/file');
+const path = require('path');
+const chalk = require('chalk');
+
 
 exports.command = 'init [dir]'
 exports.desc = 'Initialize kitty dir.'
 exports.builder = {
   dir: {
-    default: '.'
+    default: './kitty'
   }
 }
 exports.handler = function (argv) {
-  console.table(argv);
-  console.log('init called for dir', argv.dir)
+  try {
+    const sourceFile = path.resolve(__dirname, '../templates');
+    copy(sourceFile, argv.dir);
+    console.log(chalk.green('Kitty dir initialized successfully!'));
+    console.log('');
+    console.log(`Please read the ${chalk.cyan('README.md')} file in the kitty dir.`)  
+  } catch (error) {
+    console.log(chalk.red(error.message));
+  }
 }
