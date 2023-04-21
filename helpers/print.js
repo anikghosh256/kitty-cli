cd03e002fd5e151b84d62f881a86eabfc87fb1b1
Anik Ghosh <anikghosh46.bd@gmail.com>
2023-04-22 01:45:36 +0600

    'showHelp' added and logo updated.

--- helpers/print.js
+++ helpers/print.js
@@ -1,10 +1,28 @@
+const chalk = require('chalk')
+const version = require('../package.json').version
+
 exports.logo = () => {  
-  console.log(chalk.gray(' _    _ _   _         '))
-  console.log(chalk.gray('| |  (_) | | |        '))
-  console.log(chalk.gray('| | ___| |_| |_ _   _ '))
-  console.log(chalk.gray('| |/ / | __| __| | | |'))
-  console.log(chalk.gray('|   <| | |_| |_| |_| |'))
-  console.log(chalk.gray('|_|\_\_|\__|\__|\__, |'))
-  console.log(chalk.gray('                 __/ |'))
-  console.log(chalk.gray('                |___/ '))
+  console.log(chalk.green(' _    _ _   _         '))
+  console.log(chalk.green('| |  (_) | | |        '))
+  console.log(chalk.green('| | ___| |_| |_ _   _ '))
+  console.log(chalk.green('| |/ / | __| __| | | |'))
+  console.log(chalk.green('|   <| | |_| |_| |_| |'))
+  console.log(chalk.green('|_|\_\_|\__|\__|\__, |'))
+  console.log(chalk.green('                 __/ |'))
+  console.log(chalk.green('                |___/ '))
+  console.log(chalk.cyan(`                v${version}`))
+  console.log('')
+}
+
+exports.showHelp = () => {
+  console.log(chalk.gray('Usage: kitty <command> [options]'))
+  console.log('')
+  console.log(chalk.gray('Commands:'))
+  console.log(chalk.gray('  init             Initialize kitty dir.'))
+  console.log(chalk.gray('  create [name]    Create a kitty component.'))
+  console.log(chalk.gray('  help             Show help.'))
+  console.log('')
+  console.log(chalk.gray('For more information, run any command with the `--help` flag:'))
+  console.log(chalk.gray('  kitty create --help'))
+  console.log(chalk.gray('  kitty init --help'))
 }
