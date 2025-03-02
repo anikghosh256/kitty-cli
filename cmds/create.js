736432690c45dbe8c0af2edb7b26dc01ea1b5310
Anik Ghosh <52236930+anikghosh256@users.noreply.github.com>
2025-03-02 16:37:29 +0600

    feat: folder support (#16)
    * feat: add "folder" support
    
    * chore: update docs and version
    
    * fix: version number

--- cmds/create.js
+++ cmds/create.js
@@ -8,90 +8,142 @@ exports.desc = 'Create a kitty component.';
 
 exports.handler = function (argv) {
 	try {
-		if (argv.moduleName === undefined) {
+		const { moduleName, name } = argv;
+		if (!moduleName)
 			throw new Error('Please provide a name for the component.');
-		}
+		if (!name)
+			throw new Error(
+				'Please provide a name for the component instance.'
+			);
+
 		const projectPath = process.cwd();
 		const configFilePath = path.resolve(
 			projectPath,
-			`./kitty/${argv.moduleName}/config.json`
+			`./kitty/${moduleName}/config.json`
 		);
+		const config = JSON.parse(
+			validateFile(
+				configFilePath,
+				`Component ${moduleName} does not exist.`
+			)
+		);
+		const inputObject = prepareInputs(config.inputs, argv);
 
-		// check if config file exists
-		if (!fs.existsSync(configFilePath)) {
-			throw new Error(`Component ${argv.moduleName} does not exist.`);
-		}
-
-		const config = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
-
-		// check source file
-		if (config.sourceFile === undefined) {
-			throw new Error(`Source file not defined in config file.`);
-		}
-
-		// check source file exists
-		const sourceFile = path.resolve(
+		const outputFolder = path.resolve(
 			projectPath,
-			`./kitty/${argv.moduleName}/${config.sourceFile}`
+			config.dir.replace('${name}', name)
 		);
-		if (!fs.existsSync(sourceFile)) {
-			throw new Error(`Source file ${config.sourceFile} does not exist.`);
-		}
+		ensureDirExists(outputFolder);
 
-		// check required fields and make object
-		const inputKeys = Object.keys(config.inputs);
-		const inputObject = {};
+		config.type === 'folder'
+			? processFolder(
+					config,
+					inputObject,
+					argv,
+					projectPath,
+					configFilePath
+			  )
+			: processSingleFile(config, inputObject, argv, projectPath);
 
-		for (let i = 0; i < inputKeys.length; i++) {
-			if (argv[inputKeys[i]] === undefined) {
-				if (config.inputs[inputKeys[i]].default === undefined) {
-					throw new Error(
-						`Please provide a value for ${inputKeys[i]}.`
-					);
-				}
-				argv[inputKeys[i]] = config.inputs[inputKeys[i]].default;
-			}
-			inputObject[inputKeys[i]] = argv[inputKeys[i]];
-		}
-
-		// check name field
-		if (argv.name === undefined) {
-			throw new Error(
-				`Please provide a name for the component. It will be used as the output file name.`
-			);
-		}
-
-		// check output folder or create if not exist
-		const outputFolder = path.resolve(projectPath, config.dir);
-		if (!fs.existsSync(outputFolder)) {
-			fs.mkdirSync(outputFolder, { recursive: true });
-		}
-
-		// compile template
-		const compiled = compile(sourceFile, inputObject);
-
-		// write to output file
-		let outputFile = path.resolve(
-			projectPath,
-			`${config.dir}/${argv.name}.${config.outputExtension}`
-		);
-		if (config.outputFile) {
-			outputFile = path.resolve(
-				projectPath,
-				`${config.dir}/${compile(null, inputObject, config.outputFile)}`
-			);
-		}
-		fs.writeFileSync(outputFile, compiled);
-
-		console.log();
 		console.log(chalk.green('Component created successfully!'));
-
-		// check if config file has successMessage
-		if (config.successMessage !== undefined) {
-			console.log();
-			console.log(chalk.green(compile(null, inputObject, config.successMessage)));
-		}
+		if (config.successMessage)
+			console.log(
+				chalk.green(compile(null, inputObject, config.successMessage))
+			);
 	} catch (error) {
-		console.log(chalk.red(error.message));
+		console.error(chalk.red(error.message));
 	}
 };
+
+function validateFile(filePath, errorMessage) {
+	if (!fs.existsSync(filePath)) throw new Error(errorMessage);
+	return fs.readFileSync(filePath, 'utf8');
+}
+
+function prepareInputs(inputs = {}, argv) {
+	return Object.keys(inputs).reduce((acc, key) => {
+		acc[key] =
+			argv[key] !== undefined
+				? argv[key]
+				: inputs[key]?.default ??
+				  (() => {
+						throw new Error(`Please provide a value for ${key}.`);
+				  })();
+		return acc;
+	}, {});
+}
+
+function ensureDirExists(dir) {
+	fs.existsSync(dir) || fs.mkdirSync(dir, { recursive: true });
+}
+
+function processFolder(config, inputObject, argv, projectPath, configFilePath) {
+	const sourceFolder = path.resolve(
+		projectPath,
+		`./kitty/${argv.moduleName}`
+	);
+	const outputFolder = path.resolve(
+		projectPath,
+		config.dir.replace('${name}', argv.name)
+	);
+	const files = walkSync(sourceFolder);
+
+	files.forEach(file => {
+		if (file === configFilePath) return;
+		const relativePath = path.relative(sourceFolder, file);
+		const fileExtension = path.extname(file).slice(1);
+		const normalizedRelativePath = relativePath.replace(/\\/g, '/');
+		let outputFilePath = path.resolve(outputFolder, relativePath);
+
+		if (config.mapFileName[normalizedRelativePath]) {
+			const mappedFileName = compile(
+				null,
+				inputObject,
+				config.mapFileName[normalizedRelativePath]
+			);
+			outputFilePath = path.resolve(
+				outputFolder,
+				path.dirname(relativePath),
+				mappedFileName
+			);
+		}
+
+		ensureDirExists(path.dirname(outputFilePath));
+		config.extensions.length === 0 ||
+		config.extensions.includes(fileExtension)
+			? fs.writeFileSync(outputFilePath, compile(file, inputObject))
+			: fs.copyFileSync(file, outputFilePath);
+	});
+}
+
+function processSingleFile(config, inputObject, argv, projectPath) {
+	const sourceFile = path.resolve(
+		projectPath,
+		`./kitty/${argv.moduleName}/${config.sourceFile}`
+	);
+	validateFile(
+		sourceFile,
+		`Source file ${config.sourceFile} does not exist.`
+	);
+
+	let outputFilePath = path.resolve(
+		projectPath,
+		`${config.dir}/${argv.name}.${config.outputExtension}`
+	);
+	if (config.outputFile) {
+		outputFilePath = path.resolve(
+			projectPath,
+			`${config.dir}/${compile(null, inputObject, config.outputFile)}`
+		);
+	}
+	fs.writeFileSync(outputFilePath, compile(sourceFile, inputObject));
+}
+
+function walkSync(dir) {
+	return fs.readdirSync(dir).flatMap(file => {
+		const filePath = path.join(dir, file);
+		return fs.statSync(filePath).isDirectory()
+			? walkSync(filePath)
+			: [filePath];
+	});
+}
